ad2d0cc4f1e58e98ce392c482e619400f70c2291
houxx <hou_xiangxiang@126.com>
2020-09-11 01:38:00 +0800

    添加 id

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,9 +1,12 @@
 layui.use(['jquery'], function () {
     var $ = layui.jquery;
+    var itemIds = [];
     //一般请求后端接口拿到数据 这里只是演示
     var initData = [{"name": "张三", "msg": "今天心情很好"}, {"name": "李四", "msg": "不太开心"}];
     initData.forEach(function (value) {
-            $("#container").append(buildItem(value.name, value.msg));
+            var id = createRandom();
+            itemIds.push("#" + id);
+            $("#container").append(buildItem(value.name, value.msg, id));
         }
     );
 
@@ -11,7 +14,12 @@ layui.use(['jquery'], function () {
     var nameList = ["王二", "麻子", "张雪", "赵四", "爱新觉罗", "溥仪"];
     var msgList = ["十年生死两茫茫", "不思量", "自难忘", "千里孤坟", "无处话凄凉", "天黑了"];
     $("#add-btn").click(function () {
-        $("#container").append(buildItem(nameList[parseInt(nameList.length * Math.random())], msgList[parseInt(msgList.length * Math.random())]));
+        var id = createRandom();
+        itemIds.push("#" + id);
+        $("#container").append(buildItem(nameList[parseInt(nameList.length * Math.random())],
+            msgList[parseInt(msgList.length * Math.random())]
+            , id
+        ));
     });
 });
 
@@ -21,7 +29,16 @@ layui.use(['jquery'], function () {
  * @param msg
  * @returns {string}
  */
-function buildItem(name, msg) {
-    return "<div class='item-container'><div class='name'>" + name + "</div><div class='msg'>" + msg + "</div></div>";
+function buildItem(name, msg, id) {
+    return "<div class='item-container' id='" + id + "'><div class='name'>" + name + "</div><div class='msg'>" + msg + "</div></div>";
+
+}
+
+/**
+ * 生成一个随机数
+ * @returns {number}
+ */
+function createRandom() {
+    return parseInt(100000000 * Math.random());
 
 }
